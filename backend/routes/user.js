c14ed505bc8acc78f9842b61eb449492fc4dc5b2
Ayne Abreham Alemayehu <aynuman19@gmail.com>
2024-10-29 11:37:28 +0300

    build: added vercel deployment file2

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 const registerController = require("../controllers/registerController");
-const checkPermissions = require("../middleware/Authorization");
+const checkPermissions = require("../middleware/authorization");
 
 router.get("/",checkPermissions("read", "roles"),userController.handleGetAllUsers);
 router.post("/",checkPermissions("create", "users"),registerController.handleAddAdmin);
